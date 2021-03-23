3de722d1cfc8a7c8b96a9963e423351e5607cd5c
Boanerges1996 <samsonnkrumah253@gmail.com>
2021-03-24 02:08:22 +0530

    unifarme logo

---
+++ lib/src/screens/unifarmeLogo.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UnifarmeLogo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      "assets/logos/logo.png",
+      width: 120.0,
+      height: 120.0,
+    );
+  }
+}
