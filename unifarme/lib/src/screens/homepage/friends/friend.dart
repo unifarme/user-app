fa0454bb8c7dfe365bdef466ac59c45c89aaf4bc
Suraj Mani <tech.smani@gmail.com>
2021-03-31 01:07:18 +0530

    Updated Friend Section

--- unifarme/lib/src/screens/homepage/friends/friend.dart
+++ unifarme/lib/src/screens/homepage/friends/friend.dart
@@ -5,13 +5,131 @@ class Friend extends StatefulWidget {
   _FriendState createState() => _FriendState();
 }
 
-class _FriendState extends State<Friend> {
+class _FriendState extends State<Friend>
+    with SingleTickerProviderStateMixin, AutomaticKeepAliveClientMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, initialIndex: 0, length: 2);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      child: Text("Friends"),
+    super.build(context);
+    return Scaffold(
+      appBar: TabBar(
+        controller: _tabController,
+        indicatorColor: Colors.tealAccent,
+        labelColor: Colors.teal,
+        unselectedLabelColor: Theme.of(context).textTheme.caption.color,
+        isScrollable: false,
+        tabs: <Widget>[
+          Tab(
+            text: "Friends",
+          ),
+          Tab(
+            text: "Requests",
+          ),
+        ],
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          ListView.separated(
+            padding: EdgeInsets.all(10),
+            separatorBuilder: (BuildContext context, int index) {
+              return Align(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  height: 0.5,
+                  width: MediaQuery.of(context).size.width / 1.3,
+                  child: Divider(),
+                ),
+              );
+            },
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.teal,
+                  radius: 25,
+                ),
+                title: Text(
+                  'Name',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.teal),
+                ),
+                subtitle: Text(
+                  'Message',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                trailing: Column(
+                  children: [
+                    Text("5 min ago"),
+                    CircleAvatar(
+                      radius: 10,
+                      backgroundColor: Colors.tealAccent,
+                      child: Text(
+                        '2',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
+          ListView.separated(
+            padding: EdgeInsets.all(10),
+            separatorBuilder: (BuildContext context, int index) {
+              return Align(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  height: 0.5,
+                  width: MediaQuery.of(context).size.width / 1.3,
+                  child: Divider(),
+                ),
+              );
+            },
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.teal,
+                  radius: 25,
+                ),
+                title: Text(
+                  'Name',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.teal),
+                ),
+                subtitle: Text(
+                  'Message',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                trailing: Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.tealAccent,
+                      radius: 15,
+                      child: Text(
+                        '+',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.black),
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
