583ab26e7607593d645bfff96933376eb61eb8e4
Boanerges1996 <samsonnkrumah253@gmail.com>
2021-03-31 21:03:59 +0530

    Signup backend connection done left with number

--- unifarme/lib/components/rounded_input_field.dart
+++ unifarme/lib/components/rounded_input_field.dart
@@ -6,17 +6,21 @@ class RoundedInputField extends StatelessWidget {
   final String hintText;
   final IconData icon;
   final ValueChanged<String> onChanged;
-  const RoundedInputField({
-    Key key,
-    this.hintText,
-    this.icon = Icons.person,
-    this.onChanged,
-  }) : super(key: key);
+  final FormFieldValidator<String> validator;
+
+  const RoundedInputField(
+      {Key key,
+      this.hintText,
+      this.icon = Icons.person,
+      this.onChanged,
+      this.validator})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextFieldContainer(
-      child: TextField(
+      child: TextFormField(
+        validator: validator,
         onChanged: onChanged,
         cursorColor: kPrimaryColor,
         decoration: InputDecoration(
--- unifarme/lib/src/screens/signup/components/body.dart
+++ unifarme/lib/src/screens/signup/components/body.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:loading/indicator/ball_spin_fade_loader_indicator.dart';
+import 'package:loading/loading.dart';
 import 'package:provider/provider.dart';
 import 'package:unifarme/constants/colors.dart';
 import 'package:unifarme/src/providers/googleSignInProvider.dart';
 import 'package:unifarme/src/providers/userProvider.dart';
+import 'package:unifarme/src/request/user/user.dart';
 import 'package:unifarme/src/screens/signup/components/background.dart';
 import 'package:unifarme/components/already_have_an_account_acheck.dart';
 import 'package:unifarme/components/rounded_button.dart';
@@ -13,11 +17,30 @@ import 'package:unifarme/components/rounded_password_field.dart';
 import 'package:unifarme/src/screens/signup/components/or_divider.dart';
 import 'package:unifarme/src/screens/signup/components/social_icon.dart';
 import 'package:unifarme/src/screens/unifarmeLogo.dart';
+import 'package:email_validator/email_validator.dart' as vd;
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   const Body({
     Key key,
   }) : super(key: key);
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool clickedSignup = false;
+  String email = "";
+  String password = "";
+  String name = "";
+
+  final passwordValidator = MultiValidator([
+    RequiredValidator(errorText: 'password is required'),
+    MinLengthValidator(8, errorText: 'password must be at least 8 digits long'),
+    PatternValidator(r'(?=.*?[#?!@$%^&*-])',
+        errorText: 'passwords must have at least one special character')
+  ]);
 
   @override
   Widget build(BuildContext context) {
@@ -38,70 +61,163 @@ class Body extends StatelessWidget {
       );
     }
     Size size = MediaQuery.of(context).size;
-    return Background(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            UnifarmeLogo(),
-            Positioned(
-                child: Container(
-              child: Center(
-                child: Text(
-                  "Sign up",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 45,
-                      fontWeight: FontWeight.bold),
+    return Scaffold(
+      body: Background(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                UnifarmeLogo(),
+                Positioned(
+                    child: Container(
+                  child: Center(
+                    child: Text(
+                      "Sign up",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 45,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                )),
+                SizedBox(height: size.height * 0.03),
+                RoundedInputField(
+                  hintText: "Your Name",
+                  onChanged: (value) {
+                    name = value;
+                  },
+                  validator: (name) {
+                    if (name.length == 0) {
+                      return "Name field required";
+                    }
+                    return null;
+                  },
                 ),
-              ),
-            )),
-            SizedBox(height: size.height * 0.03),
-            RoundedInputField(
-              hintText: "Your Name",
-              onChanged: (value) {},
+                RoundedInputField(
+                  hintText: "Your Email",
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  validator: (email) {
+                    if (vd.EmailValidator.validate(email)) {
+                      return null;
+                    } else {
+                      return "Please enter a valid email";
+                    }
+                  },
+                ),
+                RoundedPasswordField(
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  validator: passwordValidator,
+                ),
+                RoundedButton(
+                  text: "SIGN UP",
+                  press: () async {
+                    setState(() {
+                      clickedSignup = true;
+                    });
+                    if (_formKey.currentState.validate()) {
+                      _formKey.currentState.save();
+                      UserRequest userReq = UserRequest(
+                        email: email,
+                        name: name,
+                        password: password,
+                      );
+
+                      var result = await userReq.signupWithCredentials();
+                      print(result);
+                      if (result == null) {
+                        final snackBar = SnackBar(
+                          content: Text(
+                            'Please check your intenet Connection',
+                            style: TextStyle(fontSize: 15),
+                            textAlign: TextAlign.center,
+                          ),
+                          backgroundColor: HexColor(
+                            green,
+                          ),
+                        );
+
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      } else {
+                        if (result["exist"] && !result["registered"]) {
+                          // For user account already exists
+                          final snackBar = SnackBar(
+                            content: Text(
+                              'User with account already exist',
+                              style: TextStyle(fontSize: 15),
+                              textAlign: TextAlign.center,
+                            ),
+                            backgroundColor: HexColor(
+                              green,
+                            ),
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        }
+                        if (result["registered"]) {
+                          // That means user has been registered successfully
+                          Navigator.of(context)
+                              .popAndPushNamed('/verify/email/cover');
+                        }
+                      }
+                      print(result);
+                    } else {}
+                    setState(() {
+                      clickedSignup = false;
+                    });
+                  },
+                ),
+                SizedBox(height: size.height * 0.03),
+                (clickedSignup ? loadingWidget() : Text("")),
+                AlreadyHaveAnAccountCheck(
+                  login: false,
+                  press: () {
+                    Navigator.of(context).pushNamed("/login");
+                  },
+                ),
+                OrDivider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SocalIcon(
+                      iconSrc: "assets/logos/Google.png",
+                      press: () async {
+                        final googleSignIn = Provider.of<GoogleSignInProvider>(
+                          context,
+                          listen: false,
+                        );
+                        final data = await googleSignIn.login();
+                        if (data != null) {
+                          userModelProv.updateUserModel(data);
+                          Navigator.of(context).popAndPushNamed('/home');
+                        }
+                      },
+                    ),
+                    SocalIcon(
+                      iconSrc: "assets/logos/Phone.png",
+                      press: () {},
+                    )
+                  ],
+                )
+              ],
             ),
-            RoundedInputField(
-              hintText: "Your Email",
-              onChanged: (value) {},
-            ),
-            RoundedPasswordField(
-              onChanged: (value) {},
-            ),
-            RoundedButton(
-              text: "SIGN UP",
-              press: () {},
-            ),
-            SizedBox(height: size.height * 0.03),
-            AlreadyHaveAnAccountCheck(
-              login: false,
-              press: () {
-                Navigator.of(context).pushNamed("/login");
-              },
-            ),
-            OrDivider(),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              SocalIcon(
-                iconSrc: "assets/logos/Google.png",
-                press: () async {
-                  final googleSignIn = Provider.of<GoogleSignInProvider>(
-                    context,
-                    listen: false,
-                  );
-                  final data = await googleSignIn.login();
-                  if (data != null) {
-                    userModelProv.updateUserModel(data);
-                    Navigator.of(context).popAndPushNamed('/home');
-                  }
-                },
-              ),
-              SocalIcon(
-                iconSrc: "assets/logos/Phone.png",
-                press: () {},
-              )
-            ])
-          ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget loadingWidget() {
+    return Center(
+      child: Loading(
+        color: Colors.pink,
+        indicator: BallSpinFadeLoaderIndicator(),
+        size: 50,
       ),
     );
   }
